7f8eab342fc5e0343ccecfd4d57af7274b28facd
Martin Rist <mlr-github@martinrist.com>
2021-10-03 17:08:14 +0100

    Customise data source to switch UI to edit mode

--- Today/Today/Controller/ReminderDetailViewController.swift
+++ Today/Today/Controller/ReminderDetailViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 class ReminderDetailViewController: UITableViewController {
   
   private var reminder: Reminder?
-  private var detailViewDataSource: ReminderDetailViewDataSource?
+  private var dataSource: UITableViewDataSource?
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    setEditing(false, animated: false)
+    navigationItem.setRightBarButton(editButtonItem, animated: false)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
+  }
+
+  override func setEditing(_ editing: Bool, animated: Bool) {
+    super.setEditing(editing, animated: animated)
     guard let reminder = reminder else {
       fatalError("No reminder found for detail view")
     }
-    detailViewDataSource = ReminderDetailViewDataSource(reminder: reminder)
-    tableView.dataSource = detailViewDataSource
+    if editing {
+      dataSource = ReminderDetailEditDataSource(reminder: reminder)
+    } else {
+      dataSource = ReminderDetailViewDataSource(reminder: reminder)
+    }
+    tableView.dataSource = dataSource
+    tableView.reloadData()
   }
 
   func configure(with reminder: Reminder) {
