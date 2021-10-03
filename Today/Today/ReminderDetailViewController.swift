a1f86bc6e42ce7a0c6691730960ade0c9c901f1a
Martin Rist <mlr-github@martinrist.com>
2021-10-03 16:25:26 +0100

    Extract data source from ReminderDetailViewController

--- Today/Today/ReminderDetailViewController.swift
+++ Today/Today/ReminderDetailViewController.swift
@@ -8,60 +8,21 @@
 import UIKit
 
 class ReminderDetailViewController: UITableViewController {
+  
+  private var reminder: Reminder?
+  private var detailViewDataSource: ReminderDetailViewDataSource?
 
-  enum ReminderRow: Int, CaseIterable {
-    case title
-    case date
-    case time
-    case notes
-
-    func displayText(for reminder: Reminder?) -> String? {
-      switch self {
-      case .title:
-        return reminder?.title
-      case .date:
-        return reminder?.dueDate.description
-      case .time:
-        return reminder?.dueDate.description
-      case .notes:
-        return reminder?.notes
-      }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    guard let reminder = reminder else {
+      fatalError("No reminder found for detail view")
     }
-
-    var cellImage: UIImage? {
-      switch self {
-      case .title:
-        return nil
-      case .date:
-        return UIImage(systemName: "calendar.circle")
-      case .time:
-        return UIImage(systemName: "clock")
-      case .notes:
-        return UIImage(systemName: "square.and.pencil")
-      }
-    }
+    detailViewDataSource = ReminderDetailViewDataSource(reminder: reminder)
+    tableView.dataSource = detailViewDataSource
   }
-
-  var reminder: Reminder?
 
   func configure(with reminder: Reminder) {
     self.reminder = reminder
   }
 
-}
-
-extension ReminderDetailViewController {
-  static let reminderDetailCellIdentifier = "ReminderDetailCell"
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    ReminderRow.allCases.count
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderDetailCellIdentifier, for: indexPath)
-    let row = ReminderRow(rawValue: indexPath.row)
-    cell.textLabel?.text = row?.displayText(for: reminder)
-    cell.imageView?.image = row?.cellImage
-    return cell
-  }
 }
