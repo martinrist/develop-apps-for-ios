7e6c0d037a1143978867a3f1e32aa86076c88a89
Martin Rist <mlr-github@martinrist.com>
2021-10-05 12:23:54 +0100

    Listen for changes to fields and update the edit UI and reminder struct
    This doesn't actually save the data back yet...

--- Today/Today/VIew/EditNotesCell.swift
+++ Today/Today/VIew/EditNotesCell.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class EditNotesCell: UITableViewCell {
 
+  typealias NotesChangeAction = (String) -> Void
+
   @IBOutlet var notesTextView: UITextView!
 
-  func configure(notes: String?) {
+  private var notesChangeAction: NotesChangeAction?
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    notesTextView.delegate = self
+  }
+
+  func configure(notes: String?, changeAction: NotesChangeAction?) {
     notesTextView.text = notes
+    self.notesChangeAction = changeAction
+  }
+}
+
+extension EditNotesCell: UITextViewDelegate {
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    if let originalText = textField.text {
+      let changedText = (originalText as NSString).replacingCharacters(in: range, with: string)
+      notesChangeAction?(changedText)
+    }
+    return true
   }
 }
