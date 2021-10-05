38962fcd9002d692b60065259e78fbfcb6ae9892
Martin Rist <mlr-github@martinrist.com>
2021-10-05 12:29:23 +0100

    Update view reminder screen when edited / cancelled

--- Today/Today/Controller/ReminderDetailViewController.swift
+++ Today/Today/Controller/ReminderDetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ReminderDetailViewController: UITableViewController {
   
   private var reminder: Reminder?
+  private var tempReminder: Reminder?
   private var dataSource: UITableViewDataSource?
 
   override func viewDidLoad() {
@@ -26,6 +27,7 @@ class ReminderDetailViewController: UITableViewController {
     }
     if editing {
       dataSource = ReminderDetailEditDataSource(reminder: reminder) { reminder in
+        self.tempReminder = reminder
         self.editButtonItem.isEnabled = true
       }
       navigationItem.title = NSLocalizedString("Edit Reminder", comment: "edit reminder nav title")
@@ -33,7 +35,13 @@ class ReminderDetailViewController: UITableViewController {
                                                          target: self,
                                                          action: #selector(cancelButtonTrigger))
     } else {
-      dataSource = ReminderDetailViewDataSource(reminder: reminder)
+      if let tempReminder = tempReminder {
+        self.reminder = tempReminder
+        self.tempReminder = nil
+        dataSource = ReminderDetailViewDataSource(reminder: tempReminder)
+      } else {
+        dataSource = ReminderDetailViewDataSource(reminder: reminder)
+      }
       navigationItem.title = NSLocalizedString("View Reminder", comment: "View reminder nav title")
       navigationItem.leftBarButtonItem = nil
       editButtonItem.isEnabled = true
